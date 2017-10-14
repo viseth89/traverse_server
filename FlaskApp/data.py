4e5449b9f6ce420a41a73f7a1bd2603c9f6b7f94
Ubuntu <ubuntu@ip-172-31-47-98.us-east-2.compute.internal>
2017-10-14 23:46:41 +0000

    Wrapping up first Video

---
+++ FlaskApp/data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'name':'Article One',
+            'title':'jordan',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'Brad Traversy',
+            'create_date':'04-25-17'
+        },
+        {
+            'id':2,
+            'name':'Article Two',
+            'title':'pippen',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'Brad Traversy',
+            'create_date':'04-25-17'
+        },
+        {
+            'id':3,
+            'name':'Article Three',
+            'title':'rodman',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'Brad Traversy',
+            'create_date':'04-25-17'
+        }
+
+    ]
+    return articles
